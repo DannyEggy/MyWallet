c9951808b4261b2f33568e2908a096a5cacd76b0
Admins <thanhdanh2605@gmail.com>
2023-09-25 23:22:20 +0700

    Update 25/9/2023

--- app/src/main/java/com/tdtu/mywallet/main_fragment/AnalysisFragment.java
+++ app/src/main/java/com/tdtu/mywallet/main_fragment/AnalysisFragment.java
@@ -1,15 +1,22 @@
 package com.tdtu.mywallet.main_fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -66,26 +73,54 @@ public class AnalysisFragment extends Fragment {
     private TabLayout tabLayout;
     private SearchView searchView;
     private ViewPager2 viewPager2;
+
+    // Tittle for 12 recent months and a previous year
+    private TextView recent_1_month_all;
+    private TextView recent_2_month_all;
+    private TextView recent_3_month_all;
+    private TextView recent_4_month_all;
+    private TextView recent_5_month_all;
+    private TextView recent_6_month_all;
+    private TextView recent_7_month_all;
+    private TextView recent_8_month_all;
+    private TextView recent_9_month_all;
+    private TextView recent_10_month_all;
+    private TextView recent_11_month_all;
+    private TextView recent_12_month_all;
+    private TextView recent_1_year;
+
+    // RecyclerView for recent 12 months and a previous year
+    private RecyclerView rv_1_month_all;
+    private RecyclerView rv_2_month_all;
+    private RecyclerView rv_3_month_all;
+    private RecyclerView rv_4_month_all;
+    private RecyclerView rv_5_month_all;
+    private RecyclerView rv_6_month_all;
+    private RecyclerView rv_7_month_all;
+    private RecyclerView rv_8_month_all;
+    private RecyclerView rv_9_month_all;
+    private RecyclerView rv_10_month_all;
+    private RecyclerView rv_11_month_all;
+    private RecyclerView rv_12_month_all;
+    private RecyclerView rv_1_year;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_analysis, container, false);
 
-        tabLayout = view.findViewById(R.id.tabLayoutAnalysis);
-        searchView = view.findViewById(R.id.searchAnalysis);
-        viewPager2 = view.findViewById(R.id.transactionHistory);
+        // View Binding
+        viewBindingAnalysis(view);
+        viewBindingAllPage(view);
 
-//        viewPager2.setPageTransformer(new ViewPager2.PageTransformer() {
-//            @Override
-//            public void transformPage(@NonNull View page, float position) {
-//                page.setTranslationX(0);
-//                page.setAlpha(0);
-//            }
-//        });
 
+        // Set viewpager2 adapter
+        // Setting 3 tab in viewpager2
         HistoryViewPager2Adapter historyViewPager2Adapter = new HistoryViewPager2Adapter(getActivity());
         viewPager2.setAdapter(historyViewPager2Adapter);
+
         TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout,viewPager2,false,false,  (tab, position) -> {
             switch (position) {
                 case 0:
@@ -100,8 +135,54 @@ public class AnalysisFragment extends Fragment {
             }
         });
         tabLayoutMediator.attach();
+        TabLayout.Tab defaultTab = tabLayout.getTabAt(0);
+        defaultTab.select();
 
 
-            return view;
+
+
+        return view;
     }
+
+    public void viewBindingAnalysis(View view){
+        searchView = view.findViewById(R.id.searchAnalysis);
+        tabLayout = view.findViewById(R.id.tabLayoutAnalysis);
+        viewPager2 = view.findViewById(R.id.transactionHistory);
+    }
+
+    public void viewBindingAllPage(View view){
+        //TextView view binding
+        recent_1_month_all = view.findViewById(R.id.recent_1_month_all);
+        recent_2_month_all = view.findViewById(R.id.recent_2_month_all);
+        recent_3_month_all = view.findViewById(R.id.recent_3_month_all);
+        recent_4_month_all = view.findViewById(R.id.recent_4_month_all);
+        recent_5_month_all = view.findViewById(R.id.recent_5_month_all);
+        recent_6_month_all = view.findViewById(R.id.recent_6_month_all);
+        recent_7_month_all = view.findViewById(R.id.recent_7_month_all);
+        recent_8_month_all = view.findViewById(R.id.recent_8_month_all);
+        recent_9_month_all = view.findViewById(R.id.recent_9_month_all);
+        recent_10_month_all = view.findViewById(R.id.recent_10_month_all);
+        recent_11_month_all = view.findViewById(R.id.recent_11_month_all);
+        recent_12_month_all = view.findViewById(R.id.recent_12_month_all);
+        recent_1_year = view.findViewById(R.id.recent_1_year);
+
+        // RecyclerView view binding
+        rv_1_month_all = view.findViewById(R.id.rv_1_month_all);
+        rv_2_month_all = view.findViewById(R.id.rv_2_month_all);
+        rv_3_month_all = view.findViewById(R.id.rv_3_month_all);
+        rv_4_month_all = view.findViewById(R.id.rv_4_month_all);
+        rv_5_month_all = view.findViewById(R.id.rv_5_month_all);
+        rv_6_month_all = view.findViewById(R.id.rv_6_month_all);
+        rv_7_month_all = view.findViewById(R.id.rv_7_month_all);
+        rv_8_month_all = view.findViewById(R.id.rv_8_month_all);
+        rv_9_month_all = view.findViewById(R.id.rv_9_month_all);
+        rv_10_month_all = view.findViewById(R.id.rv_10_month_all);
+        rv_11_month_all = view.findViewById(R.id.rv_11_month_all);
+        rv_12_month_all = view.findViewById(R.id.rv_12_month_all);
+        rv_1_year = view.findViewById(R.id.rv_1_year);
+
+
+    }
+
+
 }
